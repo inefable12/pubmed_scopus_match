121a30700853366089ab73b02d4e6b985cf12f5d
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-12-14 16:25:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,28 +37,28 @@ def main():
             combined_df = pd.concat([df1, df2], ignore_index=True)
             
             # Eliminar filas duplicadas basándose en la columna 'DOI'
-            combined_df = combined_df.drop_duplicates(subset=['DOI'], keep='first')
+            #combined_df = combined_df.drop_duplicates(subset=['DOI'], keep='first')
             
             # Mover la columna 'DOI' a la primera posición
-            cols = list(combined_df.columns)
-            cols.insert(0, cols.pop(cols.index('DOI')))
-            combined_df = combined_df[cols]
+            #cols = list(combined_df.columns)
+            #cols.insert(0, cols.pop(cols.index('DOI')))
+            #combined_df = combined_df[cols]
             
             # Guardar el archivo combinado como match.csv
             combined_df.to_csv('match.csv', index=False)
             
-            st.success("El archivo match.csv ha sido creado con éxito.")
+            #st.success("El archivo match.csv ha sido creado con éxito.")
             
             # Detectar y guardar las filas que estaban duplicadas antes de la eliminación
-            duplicated_df = pd.concat([df1, df2], ignore_index=True)
-            duplicated_df = duplicated_df[duplicated_df.duplicated(subset=['DOI'], keep=False)]
+            #duplicated_df = pd.concat([df1, df2], ignore_index=True)
+            #duplicated_df = duplicated_df[duplicated_df.duplicated(subset=['DOI'], keep=False)]
             
             # Guardar los duplicados en un archivo llamado duplicado.csv
-            duplicated_df.to_csv('duplicado.csv', index=False)
+            #duplicated_df.to_csv('duplicado.csv', index=False)
             
-            st.success("El archivo duplicado.csv ha sido creado con éxito.")
+            #st.success("El archivo duplicado.csv ha sido creado con éxito.")
             
-            st.write("Vista previa de los datos combinados (sin duplicados):")
+            #st.write("Vista previa de los datos combinados (sin duplicados):")
             st.dataframe(combined_df.head())
             
         except Exception as e:
